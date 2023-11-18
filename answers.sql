2e1d848fd464dc21e58fb945cdbd9c373647ebbf
Tomas De Piano <tomasdepiano1@gmail.com>
2023-11-17 19:53:29 -0700

    Finished questions 1-3 on quiz section

--- answers.sql
+++ answers.sql
@@ -2,3 +2,7 @@
 SELECT email FROM customers ORDER BY email ASC;
 
 -- Question 2: 
+SELECT id FROM orders WHERE customer_id IN ( SELECT id FROM customers WHERE fname = 'Elizabeth' AND lname = 'Crocker');
+
+-- Question 3:
+SELECT SUM(num_cupcakes) AS sum FROM orders WHERE processed = FALSE;
